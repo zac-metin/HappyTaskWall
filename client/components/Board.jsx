93ac4c054ddd3bc3a15d939df497bc1af2b023bb
alan-jordan <alanpjordan@gmail.com>
2017-06-01 11:53:33 +1200

    firs tpush

---
+++ client/components/Board.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import Task from './Task'
+const Board = (props) => (
+  <div>
+
+
+
+
+
+
+  </div>
+)
+
+function renderTasks (task)
+
+
+export default LoadSubreddit
