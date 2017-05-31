aa20be470a3da1b96ea9e013f00f5002fe7a4ab8
Zac Metin <zac.metin@gmail.com>
2017-06-01 11:37:38 +1200

    app is done

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import {HashRouter as Router, Route } from 'react-router-dom'
 
-import LoadSubreddit from './LoadSubreddit'
-import SubredditContainer from '../containers/SubredditContainer'
+import Board from './Board'
 
 const App = () => (
   <div className='app-container'>
-    <LoadSubreddit />
-    <SubredditContainer />
+  <Router>
+  <Route path="/" exact="true" component={Board}>
+  <Board />
+  </Router>
   </div>
 )
 
