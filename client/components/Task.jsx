7197e558171bfeab88eb9aaa5340bd71fb65062e
Zac Metin <zac.metin@gmail.com>
2017-06-01 15:14:49 +1200

    stage reducers

---
+++ client/components/Task.jsx
@@ -0,0 +1,12 @@
+import React, {PropTypes} from 'react'
+
+const Task = (props) => (
+  <div>{props.taskname}</div>
+  <div>{props.}
+)
+
+Post.propTypes = {
+  title: PropTypes.string.isRequired
+}
+
+export default Post
