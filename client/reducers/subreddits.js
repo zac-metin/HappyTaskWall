f757e3aec913d6aa19999f02f9320e3ac6bf1a1a
Zac Metin <zac.metin@gmail.com>
2017-06-01 15:19:38 +1200

    reducers

---
+++ client/reducers/subreddits.js
@@ -0,0 +1,13 @@
+function subreddits (state = [], action) {
+  switch (action.type) {
+    case 'RECEIVE_POSTS':
+      return [
+        ...action.posts
+      ]
+
+    default:
+      return state
+  }
+}
+
+export default subreddits
