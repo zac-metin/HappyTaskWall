70265ec0093565790a0e0dee1882e6f86df6d852
Zac Metin <zac.metin@gmail.com>
2017-06-01 16:06:41 +1200

    index html

--- client/components/App.js
+++ client/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import {HashRouter as Router, Route} from 'react-router-dom'
+
 import Board from './Board'
 
 const App = () => (
   <div className='app-container'>
-  hello
-    <Board/>
+  <p>If you see this, app has rendered</p>
+<Router>
+<Route path='/' exact component={Board} />
+</Router>
   </div>
 )
 
--- client/components/Task.jsx
+++ client/components/Task.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Task = (props) => (
 <div>
   <div>{props.taskname}</div>
-  <div>{props.id} </div>
-  </div>
+  <div>{props.id}</div>
+</div>
 )
 
 
